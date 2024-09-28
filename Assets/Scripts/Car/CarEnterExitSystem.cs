17faae285e30f76ca4fec8941f203df76946862f
NMarcell13 <147430853+NMarcell13@users.noreply.github.com>
2024-09-29 01:04:26 +0200

    BeKiSystem
    Meg lett oldva a "beszállás" és "kiszállás".
    Be: F gomb
    Ki: G  gomb

---
+++ Assets/Scripts/Car/CarEnterExitSystem.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public MonoBehaviour CarController;
+    public Transform Car;
+    public Transform Player;
+
+    [Header("Cameras")]
+    public GameObject PlayerCam;
+    public GameObject CarCam;
+
+    public GameObject DriveUI;
+
+    bool CanDrive;
+    bool driving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CarController.enabled=false;
+        DriveUI.gameObject.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        if (Input.GetKeyDown(KeyCode.F) && CanDrive)
+        {
+            
+            CarController.enabled=true;
+            DriveUI.gameObject.SetActive(false);
+            driving=true;
+            Player.transform.SetParent(Car);
+            Player.gameObject.SetActive(false);
+            //Kamera
+            PlayerCam.gameObject.SetActive(false);
+            CarCam.gameObject.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            driving=false;
+            CarController.enabled=false;
+            Player.transform.SetParent(null);
+            Player.gameObject.SetActive(true);
+
+            //Kamera
+            PlayerCam.gameObject.SetActive(true);
+            CarCam.gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag=="Player")
+        {
+
+            DriveUI.gameObject.SetActive(true);
+            CanDrive=true;
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag=="Player")
+        {
+            DriveUI.gameObject.SetActive(false);
+            CanDrive=false;
+        }
+    }
+}
