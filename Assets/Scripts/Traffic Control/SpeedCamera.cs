2b660c4cb87963bc88c9e1050e7322e9f363577d
MarcellK06 <150803469+MarcellK06@users.noreply.github.com>
2024-10-04 14:59:38 +0200

    fixes

--- Assets/Scripts/Traffic Control/SpeedCamera.cs
+++ Assets/Scripts/Traffic Control/SpeedCamera.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpeedCamera : MonoBehaviour { 
@@ -5,17 +6,24 @@ public class SpeedCamera : MonoBehaviour {
     public Transform cameraModule;
     public float allowedSpeed;
     RaycastHit hit;
-    RaycastHit lasthit = new RaycastHit();
+    public bool active = true;
+    public float cooldown;
     void Update() {
         if (Physics.Raycast(cameraModule.position, cameraModule.forward, out hit, 30f)) {
 
             var car = hit.transform.GetComponent<SCC_Drivetrain>();
-            if (car.speed <= allowedSpeed || !car || hit.transform == lasthit.transform)
+            if (car.speed <= allowedSpeed || !car || !active)
                 return;
             var player = hit.transform.GetComponent<PlayerData>().economy;
             player.addbank((car.speed - allowedSpeed) * 10);
-            lasthit = hit;
-        } else
-            lasthit = new RaycastHit();
+            Debug.Log(player.bank);
+            StartCoroutine(setActive());
+        }
+    }
+
+    public IEnumerator setActive() {
+        active = false;
+        yield return new WaitForSeconds(cooldown);
+        active = true;
     }
 }
