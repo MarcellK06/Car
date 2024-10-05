b9059d50053c571287188855efdadbdba3478c35
MarcellK06 <150803469+MarcellK06@users.noreply.github.com>
2024-10-05 15:13:39 +0200

    Engine braking, and new map

--- Assets/Simple Car Controller/Scripts/SCC_Drivetrain.cs
+++ Assets/Simple Car Controller/Scripts/SCC_Drivetrain.cs
@@ -173,7 +173,7 @@ public class SCC_Drivetrain : MonoBehaviour {
         for (int i = 0; i < wheels.Length; i++) {
 
             if (wheels[i].isTraction)
-                wheels[i].wheelCollider.WheelCollider.motorTorque = (((engineTorque + turboState) * gearRatios[currentGear]) * (direction == 1 ? InputProcessor.inputs.throttleInput : -InputProcessor.inputs.brakeInput)) / Mathf.Clamp(totalTractionWheels, 1, 20);
+                wheels[i].wheelCollider.WheelCollider.motorTorque = (inputProcessor.inputs.throttleInput == 0 && inputProcessor.inputs.brakeInput == 0 && (speed > 1 || speed < -1 )) ? (engineTorque * gearRatios[currentGear]/4 * (speed/-speed)) : (((engineTorque + turboState) * gearRatios[currentGear]) * (direction == 1 ? InputProcessor.inputs.throttleInput : -InputProcessor.inputs.brakeInput)) / Mathf.Clamp(totalTractionWheels, 1, 20);
             else
                 wheels[i].wheelCollider.WheelCollider.motorTorque = 0f;
 
